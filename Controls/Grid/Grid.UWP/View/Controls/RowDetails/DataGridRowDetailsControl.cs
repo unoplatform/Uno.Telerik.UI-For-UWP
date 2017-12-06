fb46c31c14c767e7e52894fd3aef545933a58d9b
Jérôme Laban <jerome.laban@nventive.com>
2017-12-06 16:44:36 -0500

    wip

--- Controls/Grid/Grid.UWP/View/Controls/RowDetails/DataGridRowDetailsControl.cs
+++ Controls/Grid/Grid.UWP/View/Controls/RowDetails/DataGridRowDetailsControl.cs
@@ -1,9 +1,6 @@
 ﻿namespace Telerik.UI.Xaml.Controls.Grid.Primitives
 {
-    /// <summary>
-    /// The control that represents the row details of the DataGrid.
-    /// </summary>
-    public class DataGridRowDetailsControl : RadContentControl
+    public partial class DataGridRowDetailsControl : RadContentControl
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DataGridRowDetailsControl" /> class.
