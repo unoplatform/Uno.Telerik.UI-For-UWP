d65594dbf29b3d5e1b637b17b51ab12dce25005f
vatsashah45 <vatsashah45@gmail.com>
2025-03-03 17:19:31 -0500

    feat: Add Calendar Sample

---
+++ Controls/NewSampleApp/NewSampleApp/Presentation/Formatting.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace NewSampleApp.Presentation
+{
+	public sealed partial class Formatting : ExamplePageBase
+	{
+		public Formatting()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
