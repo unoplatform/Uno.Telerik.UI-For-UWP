5ffa850fa82bfb89b009eae05f4b252f6d404aaf
vatsashah45 <vatsashah45@gmail.com>
2025-03-04 11:27:21 -0500

    chore: update MainPage

--- Controls/NewSampleApp/NewSampleApp/Presentation/MainPage.xaml.cs
+++ Controls/NewSampleApp/NewSampleApp/Presentation/MainPage.xaml.cs
@@ -1,9 +1,37 @@
-﻿namespace NewSampleApp.Presentation;
+namespace NewSampleApp.Presentation;
 
 public sealed partial class MainPage : Page
 {
-    public MainPage()
+	public MainPage()
+	{
+		this.InitializeComponent();
+	}
+	private void NavigateToBlackoutCells_Click(object sender, RoutedEventArgs e)
+	{
+		Frame.Navigate(typeof(BlackoutCells));
+	}
+    private void NavigateToCustomEventInformation_Click(object sender, RoutedEventArgs e)
     {
-        this.InitializeComponent();
+        Frame.Navigate(typeof(CustomEventInformation));
+    }
+    private void NavigateToFirstDayOfWeek_and_WeekRule_Click(object sender, RoutedEventArgs e)
+    {
+        Frame.Navigate(typeof(FirstDayOfWeek_and_WeekRule));
+    }
+    private void NavigateToFormatting_Click(object sender, RoutedEventArgs e)
+    {
+        Frame.Navigate(typeof(Formatting));
+    }
+    private void NavigateToRemoveNavigationAnimations_Click(object sender, RoutedEventArgs e)
+    {
+        Frame.Navigate(typeof(RemoveNavigationAnimations));
+    }
+    private void NavigateToSelection_Click(object sender, RoutedEventArgs e)
+    {
+        Frame.Navigate(typeof(Selection));
+    }
+    private void NavigateToStyling_Click(object sender, RoutedEventArgs e)
+    {
+        Frame.Navigate(typeof(Styling));
     }
 }
